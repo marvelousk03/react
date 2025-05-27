16b2711719e14493ccefef9091610b8650d1cfc7
marvek16 <marvelousk1603f@gmail.com>
2025-05-27 09:00:23 +0200

    commit

--- redux/chapter_07/chapter07/src/index.js
+++ redux/chapter_07/chapter07/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import GitHub from './GitHub';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <GitHub/>
+    {/* <App /> */}
   </React.StrictMode>
 );
 
